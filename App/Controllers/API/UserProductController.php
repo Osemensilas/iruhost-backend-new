c293dbc8c3379aad6ca6b64ae49749721c4ad1e6
unknown <osemensilas@gmail.com>
2026-09-20 13:52:41 +0100

    New Commit

--- App/Controllers/API/UserProductController.php
+++ App/Controllers/API/UserProductController.php
@@ -228,6 +228,82 @@ class UserProductController{
         ]);
     }
 
+    public function EmailList(){
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+            return;
+        }
+
+        if (!isset($_SESSION['user'])){
+            echo json_encode(['status' => 'error', 'message' => 'Invalid user']);
+            return;
+        }
+
+        $stmt = $this->pdo->prepare("SELECT * FROM `products` WHERE user_id = ? AND product = ?");
+        $stmt->execute([$this->userId, 'email']);
+
+        if ($stmt->rowCount() > 0){
+            $rows = $stmt->fetchAll();
+
+            echo json_encode([
+                'status' => 'success',
+                'user' => $_SESSION['user']['name'],
+                'products' => $rows
+            ]);
+        }
+    }
+
+    public function SslList(){
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+            return;
+        }
+
+        if (!isset($_SESSION['user'])){
+            echo json_encode(['status' => 'error', 'message' => 'Invalid user']);
+            return;
+        }
+
+        $stmt = $this->pdo->prepare("SELECT * FROM `products` WHERE user_id = ? AND product = ?");
+        $stmt->execute([$this->userId, 'ssl']);
+
+        if ($stmt->rowCount() > 0){
+            $rows = $stmt->fetchAll();
+
+            echo json_encode([
+                'status' => 'success',
+                'user' => $_SESSION['user']['name'],
+                'products' => $rows
+            ]);
+        }
+    }
+
+    public function AppList() {
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+            return;
+        }
+
+        if (!isset($_SESSION['user'])){
+            echo json_encode(['status' => 'error', 'message' => 'Invalid user']);
+            return;
+        }
+
+        $stmt = $this->pdo->prepare("SELECT * FROM `products` WHERE user_id = ? AND product = ?");
+        $stmt->execute([$this->userId, 'web app']);
+
+        if ($stmt->rowCount() > 0){
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            echo json_encode([
+                'status' => 'success',
+                'user' => $_SESSION['user']['name'],
+                'products' => $rows
+            ]);
+        }
+    }
+
     public function VerifyRenewal(){
 
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
