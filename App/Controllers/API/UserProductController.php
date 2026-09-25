df7ac0f00ce003fb631b17c5b5f8211c5e0fc57c
unknown <osemensilas@gmail.com>
2026-09-25 17:19:04 +0100

    New Commit

--- App/Controllers/API/UserProductController.php
+++ App/Controllers/API/UserProductController.php
@@ -26,8 +26,6 @@ class UserProductController{
             return;
         }
 
-        print_r($_SESSION['user']);
-
         if (!isset($_SESSION['user'])){
             echo json_encode(['status' => 'error', 'message' => 'Invalid user']);
             return;
